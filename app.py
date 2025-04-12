98dafa784bec13a7601a31350bad520be69324ee
Erik Jensen <erikalanj@gmail.com>
2025-04-11 21:58:45 -0400

    add gitignore to exclude venv

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Welcome to TuneTeller")
+st.subheader("TuneTeller is a music analysis system that allows you to filter you music by mood, not just genre.")
+
+song_title = st.text_input("Enter the song title")
+artist_name = st.text_input("Enter the artist name")
+if st.button("Submit"):
+    st.write(f"Song Title: {song_title}")
+    st.write(f"Artist Name: {artist_name}")
