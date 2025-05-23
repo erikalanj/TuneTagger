bf7e43d716e52c3fb887bfcdde01ac2babd33b49
Erik Jensen <erikalanj@gmail.com>
2025-05-23 19:31:10 -0400

    added frame class for song that will be better in long run

--- app.py
+++ app.py
@@ -15,6 +15,11 @@ cursor.execute(
 
 connection.commit()
 
+# class Song:
+#     def __init__ (self, song_title, song_artist):
+#         self.song_title = song_title
+#         self.song_artist = song_artist
+
 
 def show_database():
     cursor.execute("SELECT * FROM tunes")
@@ -28,16 +33,13 @@ def insert_song(song_title, song_artist):
     )
 
 
-def main():
-    col1, col2 = st.columns([1, 5])
-    with col1:
-        st.image("resources/images/guitar_logo.png", width=200)
-    with col2:
-        st.title("Welcome to TuneTagger")
+if __name__ == "__main__":
+    # Centered title and subtitle
+    st.title("Welcome to TuneTagger")
 
-    st.subheader(
-        "TuneTagger is a music analysis system that allows you to filter you music by mood, not just genre."
-    )
+    st.subheader("Enter any song you want and create a library")
+
+    st.write("")  # Adds a little vertical space
 
     song_title = st.text_input("Enter the song title")
     song_artist = st.text_input("Enter the artist name")
@@ -52,7 +54,3 @@ def main():
         dataframe = pd.DataFrame(data, columns=["Song Title", "Artist Name"])
         st.table(dataframe)
         connection.commit()
-
-
-if __name__ == "__main__":
-    main()
