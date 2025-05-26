ff4f2a6168cfa8fa20888971cb75898922ec2004
Erik Jensen <erikalanj@gmail.com>
2025-05-26 11:54:30 -0400

    User can now input song into library

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 import pandas as pd
 
@@ -30,8 +30,15 @@ def insert_song(song_title, song_artist):
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/index", methods=["POST", "GET"])
 def index():
+    if request.method == "POST":
+        song_title = request.form["title-to-insert"]
+        song_artist = request.form["artist-to-insert"]
+        if song_artist and song_title:
+            insert_song(song_title, song_artist)
+            connection.commit()
+
     data = show_database()
     dataframe = pd.DataFrame(data, columns=["Song Title", "Artist Name"])
     connection.commit()
