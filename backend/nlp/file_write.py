0a7c2bfd52e45bf96b04732f04b9c8798433f474
Erik Jensen <erikalanj@gmail.com>
2025-09-04 15:34:15 -0400

    renamed backend module, and reformatted database to correct desynced id's and updated ui to have dataframe deletion

--- backend/nlp/file_write.py
+++ backend/nlp/file_write.py
@@ -5,7 +5,7 @@ current_file_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_file_dir)
 sys.path.append(parent_dir)
 
-from testing.genius.genius_req import fetch_song_details, get_genius_access_token
+from backend.genius.genius_req import fetch_song_details, get_genius_access_token
 from string import Template
 
 
